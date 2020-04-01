b61323680975ac125f77a34470b01f24330ffeec
Cody Garn <garn.cody@gmail.com>
2020-04-01 17:00:18 -0500

    current progress

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super()
+
+  }
+
+  render(){
+    return(
+      <div className='App'>
+        <header className='header'>
+          <h1>Start Bootstrap</h1>
+          <nav className='nav-bar'>
+            <ul className='nav-list'>
+              <li className='nav-item'>SERVICES</li>
+              <li className='nav-item'>PORTFOLIA</li>
+              <li className='nav-item'>ABOUT</li>
+              <li className='nav-item'>TEAM</li>
+              <li className='nav-item'>CONTACT</li>
+            </ul>
+
+            {/* <ul className='mobile-nav-list'>
+              <li className='nav-item'>SERVICES</li>
+              <li className='nav-item'>PORTFOLIA</li>
+              <li className='nav-item'>ABOUT</li>
+              <li className='nav-item'>TEAM</li>
+              <li className='nav-item'>CONTACT</li>
+            </ul> */}
+          </nav>
+        </header>
+
+        <main>
+          <div className='body'>
+            <div className='welcome-text'>Welcome To Our Studio</div>
+            <div className='meet-you'>IT'S NICE TO MEET YOU</div>
+            <div className='more-button'>TELL ME MORE</div>
+          </div>
+        </main>
+
+      </div>
+    )
+  }
 }
+
 
 export default App;
