569d41c766cdb2343458f0437f883dbea30c10d0
Cody Garn <garn.cody@gmail.com>
2020-04-06 15:09:06 -0500

    completed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,17 @@ class App extends Component{
   constructor(){
     super()
 
+      // this.state={
+      //   mobileMenu: false
+      // }
+      // this.toggleMobileMenu = this.toggleMobileMenu.bind(this)
   }
+
+  // toggleMobileMenu(){
+  //   this.setState({
+  //     mobileMenu: !this.state.mobileMenu
+  //   })
+  // }
 
   render(){
     return(
@@ -16,25 +26,25 @@ class App extends Component{
           <nav className='nav-bar'>
             <ul className='nav-list'>
               <li className='nav-item'>SERVICES</li>
-              <li className='nav-item'>PORTFOLIA</li>
+              <li className='nav-item'>PORTFOLIO</li>
               <li className='nav-item'>ABOUT</li>
               <li className='nav-item'>TEAM</li>
               <li className='nav-item'>CONTACT</li>
             </ul>
-
-            {/* <ul className='mobile-nav-list'>
+            <img  className='hamburgerIcon' src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRvLYyhePefC1DbQvcpir_1p-jJkC4B1KafONvFwZCxlMiVLUwv&usqp=CAU'/>
+            {/* <ul className={`mobile-nav-list ${this.state.mobileMenu ? 'mobile-nav-list-open': null}`}>
               <li className='nav-item'>SERVICES</li>
-              <li className='nav-item'>PORTFOLIA</li>
+              <li className='nav-item'>PORTFOLIO</li>
               <li className='nav-item'>ABOUT</li>
               <li className='nav-item'>TEAM</li>
-              <li className='nav-item'>CONTACT</li>
+              <li className='nav-item'>CONTACT</li>onClick={this.toggleMobileMenu}
             </ul> */}
           </nav>
         </header>
 
         <main>
           <div className='body'>
-            <div className='welcome-text'>Welcome To Our Studio</div>
+            <div className='welcome-text'>Welcome To Our Studio!</div>
             <div className='meet-you'>IT'S NICE TO MEET YOU</div>
             <div className='more-button'>TELL ME MORE</div>
           </div>
